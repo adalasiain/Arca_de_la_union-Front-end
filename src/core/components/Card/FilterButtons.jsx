71552a2a53fab42c0464ca34b39543e120883d0e
JRodriAlarc <jona10131313@gmail.com>
2024-12-07 19:08:48 -0600

    Style(Card) Replace the Search icon and Icon Library

--- src/core/components/Card/FilterButtons.jsx
+++ src/core/components/Card/FilterButtons.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Search } from "lucide-react";
 
 const FilterButtons = ({
   onFilterChange,
@@ -85,7 +84,7 @@ const FilterButtons = ({
           onChange={handleSearch}
         />
         <div className="absolute right-3">
-          <Search size={18} className="text-orange-500" />
+          <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" className="text-orange-500" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m21 21l-4.343-4.343m0 0A8 8 0 1 0 5.343 5.343a8 8 0 0 0 11.314 11.314"/></svg>
         </div>
       </div>
     </div>
