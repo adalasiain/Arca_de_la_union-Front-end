69a72cf9244d1309449afe4243d4292a0576a22f
JRodriAlarc <jona10131313@gmail.com>
2024-12-07 19:52:08 -0600

    Style(FilterButtons) Customize the style of the buttons and search bar

--- src/core/components/Card/Card.jsx
+++ src/core/components/Card/Card.jsx
@@ -6,7 +6,7 @@ const Card = ({ peso, material, tamaño, acabado, precio }) => {
       {/* Imagen del producto */}
       <div>
         <img
-          src="/img/img.jpeg" // Asegúrate de que esta ruta sea correcta
+          src="/img/img.jpeg"
           alt="Campana"
           className="rounded-lg object-cover w-32 h-32"
         />
@@ -40,7 +40,7 @@ const Card = ({ peso, material, tamaño, acabado, precio }) => {
               </svg>
             ))}
           <svg
-            className="w-4 h-4 text-gray-200 dark:text-gray-600"
+            className="w-4 h-4 text-gray-200"
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             fill="currentColor"
@@ -49,18 +49,18 @@ const Card = ({ peso, material, tamaño, acabado, precio }) => {
             <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
           </svg>
         </div>
-        <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">
+        <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded ml-3">
           5.0
         </span>
       </div>
 
       {/* Precio y Botón */}
       <div className="flex items-center justify-between w-full mt-4">
-        <span className="text-xl font-bold text-gray-900 dark:text-white">
+        <span className="text-xl font-bold text-gray-900">
           ${precio}
         </span>
         <button
-          className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-4 py-1 text-center dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800"
+          className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-4 py-1 text-center"
         >
           Comprar
         </button>
--- src/core/components/Card/FilterButtons.jsx
+++ src/core/components/Card/FilterButtons.jsx
@@ -21,70 +21,70 @@ const FilterButtons = ({
   return (
     <div className="flex flex-wrap items-center justify-between gap-4 p-4 bg-orange-100 rounded-lg">
       {/* Botones de Filtro */}
-      <div className="flex flex-wrap gap-2">
+      <div className="flex flex-wrap gap-4">
         <button
-          className={`px-4 py-2 rounded-md ${
+          className={`px-4 py-2 rounded-full font-bold ${
             activeFilter === "all"
-              ? "bg-orange-500 text-white"
-              : "bg-white text-orange-500 hover:bg-orange-500 hover:text-white"
+              ? "bg-[#ee9f05] text-white"
+              : "bg-white text-[#ee9f05] hover:bg-[#ffb300] hover:text-white"
           }`}
           onClick={() => handleFilter("all")}
         >
-          Filtro 1
+          Todo
         </button>
         <button
-          className={`px-4 py-2 rounded-md ${
+          className={`px-4 py-2 rounded-full font-bold ${
             activeFilter === "mate"
-              ? "bg-orange-500 text-white"
-              : "bg-white text-orange-500 hover:bg-orange-500 hover:text-white"
+              ? "bg-[#ee9f05] text-white"
+              : "bg-white text-[#ee9f05] hover:bg-[#ffb300] hover:text-white"
           }`}
           onClick={() => handleFilter("mate")}
         >
-          Filtro 1
+          Mate
         </button>
         <button
-          className={`px-4 py-2 rounded-md ${
+          className={`px-4 py-2 rounded-full font-bold ${
             activeFilter === "pulido"
-              ? "bg-orange-500 text-white"
-              : "bg-white text-orange-500 hover:bg-orange-500 hover:text-white"
+              ? "bg-[#ee9f05] text-white"
+              : "bg-white text-[#ee9f05] hover:bg-[#ffb300] hover:text-white"
           }`}
           onClick={() => handleFilter("pulido")}
         >
-          Filtro 1
+          Pulido
         </button>
         <button
-          className={`px-4 py-2 rounded-md ${
-            activeFilter === "esmaltè"
-              ? "bg-orange-500 text-white"
-              : "bg-white text-orange-500 hover:bg-orange-500 hover:text-white"
+          className={`px-4 py-2 rounded-full font-bold ${
+            activeFilter === "esmalte"
+              ? "bg-[#ee9f05] text-white"
+              : "bg-white text-[#ee9f05] hover:bg-[#ffb300] hover:text-white"
           }`}
-          onClick={() => handleFilter("esmaltè")}
+          onClick={() => handleFilter("esmalte")}
         >
-          Filtro 1
+          Esmalte
         </button>
         <button
-          className={`px-4 py-2 rounded-md ${
+          className={`px-4 py-2 rounded-full font-bold ${
             activeFilter === "patina"
-              ? "bg-orange-500 text-white"
-              : "bg-white text-orange-500 hover:bg-orange-500 hover:text-white"
+              ? "bg-[#ee9f05] text-white"
+              : "bg-white text-[#ee9f05] hover:bg-[#ffb300] hover:text-white"
           }`}
           onClick={() => handleFilter("patina")}
         >
-          Filtro 1
+          Patina
         </button>
       </div>
 
       {/* Campo de Búsqueda */}
-      <div className="relative flex items-center w-full md:w-auto bg-white border border-gray-300 rounded-md">
+      <div className="flex items-center w-full md:w-auto bg-white rounded-full">
         <input
           type="text"
           placeholder={placeholder}
-          className="w-full px-4 py-2 pr-10 rounded-md focus:outline-none focus:ring-2 focus:ring-orange-500 focus:border-transparent"
+          className="w-full px-5 py-3 pr-12 rounded-full font-bold focus:outline-none focus:ring-2 focus:ring-[#ee9f05] focus:border-transparent"
           value={searchTerm}
           onChange={handleSearch}
         />
-        <div className="absolute right-3">
-          <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" className="text-orange-500" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m21 21l-4.343-4.343m0 0A8 8 0 1 0 5.343 5.343a8 8 0 0 0 11.314 11.314"/></svg>
+        <div className="absolute right-3 rounded-full bg-[#ee9f05] w-12 h-12 flex items-center justify-center">
+          <svg xmlns="http://www.w3.org/2000/svg" className="text-white w-6 h-6" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="3" d="m21 21l-4.343-4.343m0 0A8 8 0 1 0 5.343 5.343a8 8 0 0 0 11.314 11.314"/></svg>
         </div>
       </div>
     </div>
