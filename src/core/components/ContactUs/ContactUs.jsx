15a6c111c1b9c0777b1eb113fc8a7852b9819aea
JRodriAlarc <jona10131313@gmail.com>
2024-12-10 16:08:37 -0600

    Fix(ContactUs) Clear fields from form when data is submitted

--- src/core/components/ContactUs/ContactUs.jsx
+++ src/core/components/ContactUs/ContactUs.jsx
@@ -6,6 +6,7 @@ const ContactUs = () => {
     email: '',
     mensaje: ''
   });
+  const [isSent, setIsSent] = useState(false);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -25,16 +26,26 @@ const ContactUs = () => {
     }
 
     const phoneNumber = '527711980579'; // Número de teléfono destino en formato internacional
-    const text = `Hola, soy ${nombre}. Mi correo es ${email}. Mensaje: ${mensaje}`;
+    const text = `Hola, soy ${nombre}. Mi correo es: ${email}. Mensaje: ${mensaje}`;
     const url = `https://api.whatsapp.com/send/?phone=${phoneNumber}&text=${encodeURIComponent(text)}&type=phone_number&app_absent=0`;
 
+    // Redirige a WhatsApp
     window.open(url, '_blank');
+
+    // Limpia el formulario y muestra el mensaje de enviado
+    setFormData({ nombre: '', email: '', mensaje: '' });
+    setIsSent(true);
+
+    // Oculta el mensaje de enviado después de 3 segundos
+    setTimeout(() => {
+      setIsSent(false);
+    }, 3000);
   };
 
   return (
     <div className="flex flex-grow items-center justify-center bg-[url('/img/backgroundProducts.jpg')] bg-cover sm:bg-center sm:bg-no-repeat p-6">
       <div className="grid grid-cols-1 md:grid-cols-2 w-full max-w-screen-xl gap-6">
-
+        
         {/* Sección de Imagen y Descripción */}
         <div className="flex flex-col items-center justify-around">
           <a
@@ -104,10 +115,10 @@ const ContactUs = () => {
             >
               Contactar
               <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 ml-3" viewBox="0 0 1792 1792">
-                <path fill="currentColor" d="M1764 11q33 24 27 64l-256 1536q-5 29-32 45q-14 8-31 8q-11 0-24-5l-453-185l-242 295q-18 23-49 23q-13 0-22-4q-19-7-30.5-23.5T640 1728v-349l864-1059l-1069 925l-395-162q-37-14-40-55q-2-40 32-59L1696 9q15-9 32-9q20 0 36 11" />
+                <path fill="currentColor" d="M1764 11q33 24 27 64l-256 1536q-5 29-32 45q-14 8-31 8q-11 0-24-5l-453-185l-242 295q-18 23-49 23q-13 0-22-4q-19-7-30.5-23.5T640 1728v-349l864-1059l-1069 925l-395-162q-37-14-40-55q-2-40 32-59L1696 9q15-9 32-9q20 0 36 11"/>
               </svg>
             </button>
-          </form>
+          </form>          
         </div>
       </div>
     </div>
